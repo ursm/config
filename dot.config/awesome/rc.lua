b4e74e7e6c5ecf2cadc056cbd55c46f0416e02a6
Keita Urashima <ursm@ursm.jp>
2010-03-15 19:47:58 +0900

    awesome: Selenium で起動してくる Firefox がすごく邪魔

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -326,6 +326,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][9] } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
