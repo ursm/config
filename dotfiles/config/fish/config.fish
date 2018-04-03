9f6a3e0f3f9c4605d0c389d2da0cae6fd6eddcc3
Keita Urashima <ursm@ursm.jp>
2018-04-04 02:14:41 +0900

    fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,2 +1,12 @@
 set -x PATH $HOME/.anyenv/bin $PATH
-eval (command anyenv init - fish)
+source (anyenv init - fish | psub)
+
+set -x FZF_CD_COMMAND 'fd --type directory . $dir'
+set -x FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --hidden . $dir'
+set -x FZF_FIND_FILE_COMMAND 'fd . $dir'
+set -x FZF_LEGACY_KEYBINDINGS 0
+
+abbr g git
+abbr ap ansible-playbook
+abbr be 'bundle exec'
+abbr dc docker-compose
