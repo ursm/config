c32825aacf015ca0affbcfea235068ee4b829848
Keita Urashima <ursm@ursm.jp>
2018-04-04 15:42:46 +0900

    update

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,12 +1,14 @@
-set -x PATH $HOME/.anyenv/bin $PATH
+set -x PATH $HOME/bin $HOME/.anyenv/bin $HOME/.yarn/bin $PATH
+
 source (anyenv init - fish | psub)
+eval (direnv hook fish)
 
 set -x FZF_CD_COMMAND 'fd --type directory . $dir'
 set -x FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --hidden . $dir'
 set -x FZF_FIND_FILE_COMMAND 'fd . $dir'
 set -x FZF_LEGACY_KEYBINDINGS 0
 
+abbr b bundle
 abbr g git
 abbr ap ansible-playbook
-abbr be 'bundle exec'
 abbr dc docker-compose
