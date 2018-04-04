aaccd5bb40d115e4d64b67c30b0626263129ac5f
Keita Urashima <ursm@ursm.jp>
2018-04-04 16:15:33 +0900

    fisher

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,12 +1,14 @@
-set -x PATH $HOME/bin $HOME/.anyenv/bin $HOME/.yarn/bin $PATH
+set --export PATH $HOME/bin $HOME/.anyenv/bin $HOME/.yarn/bin $PATH
 
 source (anyenv init - fish | psub)
 eval (direnv hook fish)
 
-set -x FZF_CD_COMMAND 'fd --type directory . $dir'
-set -x FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --hidden . $dir'
-set -x FZF_FIND_FILE_COMMAND 'fd . $dir'
-set -x FZF_LEGACY_KEYBINDINGS 0
+set fish_greeting ''
+
+set FZF_CD_COMMAND 'fd --type directory . $dir'
+set FZF_CD_WITH_HIDDEN_COMMAND 'fd --type directory --hidden . $dir'
+set FZF_FIND_FILE_COMMAND 'fd --type file . $dir'
+set FZF_LEGACY_KEYBINDINGS 0
 
 abbr b bundle
 abbr g git
